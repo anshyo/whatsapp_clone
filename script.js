93f94ce21fac081b863621969709ff8c5013dc8a
Ansh Bhardwaj <93650767+anshyo@users.noreply.github.com>
2022-02-03 18:07:22 +0530

    a

---
+++ script.js
@@ -0,0 +1,6 @@
+const arc=document.querySelector('.left .down .chats .arc');
+const arcArea=document.querySelector('.left .arc-area');
+
+arc.addEventListener('click', () => {
+    arcArea.classList.toggle('arc-area-a');
+})
